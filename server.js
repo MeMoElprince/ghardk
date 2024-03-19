0751dada10470ffb2f83ec192be13af0e72eb676
Mustafa Elsharawy <memomeme621@gmail.com>
2024-03-19 04:33:22 +0200

    Adds: postgress connection and refactoring code

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const dotenv = require('dotenv');
-// const mongoose = require('mongoose');
 
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
 
-const port = process.env.PORT || 2000;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () => console.log(`Server is running on port: ${port}...`));
 
 process.on('unhandledRejection', err => {

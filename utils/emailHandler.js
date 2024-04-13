7271ec294f43301f5d78b5a7a691002f8a0bb9cd
Mustafa Elsharawy <memomeme621@gmail.com>
2024-04-14 00:31:31 +0200

    setup: email sender

---
+++ utils/emailHandler.js
@@ -0,0 +1,59 @@
+const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
+
+module.exports = class Email {
+    constructor(user, url) {
+        this.to = user.email;
+        this.firstName = user.firstName;
+        this.url = url;
+        this.from = 'Ghrdk <Gharadk Team>';
+    }
+
+    newTransport() {
+        if(process.env.NODE_ENV === 'production'){
+            // Sendgrid
+            return nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                    user: process.env.EMAIL_USERNAME,
+                    pass: process.env.EMAIL_PASSWORD
+                }
+            });
+        }
+
+        return nodemailer.createTransport({
+            host: process.env.EMAIL_HOST_DEV,
+            port: process.env.EMAIL_PORT_DEV,
+            auth: {
+                user: process.env.EMAIL_USERNAME_DEV,
+                pass: process.env.EMAIL_PASSWORD_DEV
+            }
+        });
+    }
+
+    async send(template, subject) {
+        // 1) Render HTML based on a pug template
+        const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+            firstName: this.firstName,
+            url: this.url,
+            subject
+        });
+
+        // 2) Define email options
+        const mailOptions = {
+            from: this.from,
+            to: this.to,
+            subject,
+            html,
+            text: htmlToText.fromString(html)
+        };
+
+        // 3) Create a transport and send email
+        await this.newTransport().sendMail(mailOptions);
+    }
+
+    async verifyAccount() {
+        await this.send('verifyAccount', 'Email Verification needed');
+    }
+}

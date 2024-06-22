d06458bc32b0813ac73167d4003d25d217346676
Mustafa Elsharawy <memomeme621@gmail.com>
2024-06-22 21:49:37 +0300

    querying for favourite products updated

--- controllers/favouriteProductController.js
+++ controllers/favouriteProductController.js
@@ -2,6 +2,10 @@ const FavouriteProduct = require('../models/favouriteProductModel');
 const Customer = require('../models/customerModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/AppError');
+const ProductItem = require('../models/productItemModel');
+const db = require('../config/database');
+const ProductImages = require('../models/productImageModel');
+
 
 const filterObj = (obj, ...allowedFields) => {
     const newObj = {};
@@ -21,6 +25,15 @@ exports.addProductToFavourite = catchAsync(async (req, res, next) => {
         }
     });
     data.customer_id = customer.id;
+    const favouriteProductTest = await FavouriteProduct.findOne({
+        where: {
+            customer_id: customer.id,
+            product_item_id: data.product_item_id
+        }
+    })
+    // we can replace the below if condition with setting 2 columns as a primary key in db
+    if(favouriteProductTest)
+        return next(new AppError('Product already in favourite', 400));
     const favouriteProduct = await FavouriteProduct.create(data);
     res.status(201).json({
         status: 'success',
@@ -37,20 +50,43 @@ exports.getAllFavouriteProducts = catchAsync(async (req, res, next) => {
         }
     });
     const customer_id = customer.id;
-    const favouriteProducts = await FavouriteProduct.findAll({
-        where: {
-            customer_id: customer_id
-        }
-    });
+    // const favouriteProducts = await FavouriteProduct.findAll({
+    //     where: {
+    //         customer_id: customer_id
+    //     }
+    // });
+
+    // query with sql language
+    let favouriteProducts = await db.query(
+        `SELECT product_items.id, products.name, products.description, product_items.product_id, product_items.vendor_id, product_items.quantity, product_items.price 
+         FROM favourite_products 
+         JOIN product_items ON favourite_products.product_item_id = product_items.id
+         JOIN products ON product_items.product_id = products.id
+         WHERE customer_id = ${customer_id}
+         `
+    );
+    favouriteProducts = favouriteProducts[0];
+    for(let i = 0; i < favouriteProducts.length; i++) {
+        const productImages = await db.query(
+            `
+                Select images.url as image_url, images.remote_id as image_id 
+                FROM product_images
+                JOIN images ON product_images.image_id = images.id
+                WHERE product_item_id = ${favouriteProducts[i].id}
+            `
+        );
+        console.log(productImages[0]);
+        favouriteProducts[i].images = productImages[0];
+    }
+
     res.status(200).json({
         status: 'success',
-        data: {
-            favouriteProducts
-        }
+        data: favouriteProducts
     });
 });
 
 exports.deleteFavouriteProduct = catchAsync(async (req, res, next) => {
+    // show related product_item_id
     const favouriteProduct = await FavouriteProduct.findOne({
         where: {
             id: req.params.id

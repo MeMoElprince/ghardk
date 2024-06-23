a1f9949a655a3769275a9d45a8a55063212da2bc
Mustafa Elsharawy <memomeme621@gmail.com>
2024-06-23 21:17:37 +0300

    get my cart done

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -2,11 +2,11 @@ const CartProduct = require('../models/cartProductModel');
 const ProductItem = require('../models/productItemModel');
 const Customer = require('../models/customerModel');
 const Cart = require('../models/cartModel');
-
+const db = require('../config/database');
 const color = require('../utils/colors');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/AppError');
-
+const ProductImage = require('../models/productImageModel');
 
 const filterObj = (obj, ...allowedFields) => {
     const newObj = {};
@@ -90,6 +90,11 @@ exports.updateProductInMyCart = catchAsync(async (req, res, next) => {
     if(!product_item) {
         return next(new AppError('Product not found', 404));
     }
+    if(data.quantity < 1) {
+        return next(new AppError('Invalid Quantity', 400));
+    }
+    if(product_item.quantity < data.quantity)
+        return next(new AppError('Vendor can\'t prepare that much of quantity', 400));
     data.price = product_item.price * data.quantity;
     await cartProduct.update(data);
     res.status(200).json({
@@ -114,16 +119,56 @@ exports.getMyCart = catchAsync(async (req, res, next) => {
     if(!cart) {
         return next(new AppError('Cart not found', 404));
     }
-    const cartProducts = await CartProduct.findAll({
-        where: {
-            cart_id: cart.id
-        }
-    });
+    let cartProducts = await db.query(
+        `
+            SELECT 
+                cp.id, 
+                cp.quantity, 
+                cp.price AS total_price, 
+                pi.id as product_item_id, 
+                p.name, 
+                pi.price as product_price, 
+                p.description,
+                u.first_name as vendor_first_name,
+                u.last_name as vendor_last_name,
+                u.email as vendor_email,
+                cp."createdAt",
+                cp."updatedAt"
+            FROM 
+                cart_products cp
+            JOIN 
+                product_items pi ON cp.product_item_id = pi.id
+            JOIN 
+                products p ON pi.product_id = p.id
+            JOIN 
+                vendors v ON pi.vendor_id = v.id
+            JOIN 
+                users u ON v.user_id = u.id
+            WHERE 
+                cp.cart_id = ${cart.id}
+        `
+    );
+    cartProducts = cartProducts[0];
+    for(let i = 0; i < cartProducts.length; i++) {
+        let productImages = await db.query(
+            `
+                SELECT
+                    i.url AS image_url,
+                    i.remote_id AS image_id
+                FROM
+                    product_images pi
+                JOIN
+                    images i ON pi.image_id = i.id
+                WHERE
+                    pi.product_item_id = ${cartProducts[i].product_item_id}
+            `
+        );
+        productImages = productImages[0];
+        cartProducts[i].images = productImages;
+    }
     res.status(200).json({
         status: 'success',
-        data: {
-            cartProducts
-        }
+        cartProducts
     });
 });
 
